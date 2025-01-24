a48db4b08972fdf37ac7a8cf172981a724ce5de9
ezja <ezra@beep.nl>
2025-01-24 19:51:02 +0100

    Update .zshrc for fzf-tab etc

--- orlo/setup.sh
+++
@@ -1,5 +0,0 @@
-sudo apt install zsh zsh-antigen bat fzy fzf -y 
-source /usr/share/zsh-antigen/antigen.zsh
-antigen restore ~/dotfiles/orlo/antigen-snapshot
-antigen init ~/dotfiles/orlo/.antigenrc
-antigen cache-gen
